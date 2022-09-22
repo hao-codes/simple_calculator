22d7d25803259830f603c37bf4383b930997de8d
hao-codes <59805422+hao-codes@users.noreply.github.com>
2022-09-22 16:22:59 +0200

    worked on showing clicked numbers/operators, clearing button

--- main.js
+++ main.js
@@ -20,15 +20,15 @@ function multiply(num1, num2) {
 };
 
 function divide(num1, num2) {
-    return num1/num2;
+    return num1 / num2;
 };
 
 function operate(operator, num1, num2) {
     return operator == "+" ? add(num1, num2)
-            : operator == "-" ? subtract(num1, num2)
+        : operator == "-" ? subtract(num1, num2)
             : operator == "*" ? multiply(num1, num2)
-            : operator == "/" ? divide(num1, num2)
-            : "invalid operator"
+                : operator == "/" ? divide(num1, num2)
+                    : "invalid operator"
 };
 
 
@@ -38,13 +38,13 @@ let operation;
 let num1 = document.querySelectorAll(".digit");
 
 let display = document.getElementById('display');
+console.log(display.innerHTML);
 
 
-
-for(key in num1) {
+/* for(key in num1) {
     console.log(num1[key].innerHTML)
     num1[key].addEventListener('click', fillDisplay(num1[key]));
-};
+}; */
 
 
 
@@ -54,16 +54,51 @@ console.log(typeof num1);
 let num2;
 
 let input;
+console.log("----")
+console.log(display.innerHTML)
+
+
+let digits = document.querySelectorAll('button')
+
+digits.forEach(function (e) {
+
+    e.addEventListener('click', function () {
+        console.log(e.className);
+        console.log(e.innerHTML);
+
+        if (e.id == "clear-btn") {
+            display.innerHTML = "";
+        }
 
 
 
-function fillDisplay(button) {
-    display.innerHTML = button.innerHTML;
+        else if (e.className == "digit" || e.className == "operator") {
+            if (display.innerHTML == "___") {
+                display.innerHTML = e.innerHTML;
+
+            } else {
+
+                display.innerHTML += e.innerHTML;
+            }
+        } else if ( e.id == 'delete-btn') {
+            console.log("delete");
+        }}
+    );
+
+    });
+
+
+
+function fillDisplay() {
+    let digits = document.body.getElementsByClassName('digit')
+    console.log(typeof digits);
+    console.log(digits);
+    console.log(digits[0]);
+    console.log(digits[0].innerHTML);
+
+    //    display.innerHTML = button.innerHTML;
 };
 
 /* function saveOperation {
 
 } */
-
-
-
