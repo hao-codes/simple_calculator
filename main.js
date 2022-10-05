3366015ddae7d5f88d692163c3f07fc967e02852
hao-codes <59805422+hao-codes@users.noreply.github.com>
2022-10-05 14:59:55 +0200

    fixed bug with multiplication & division button

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 // functions for  calculations
 function add(num1, num2) {
     return num1 + num2;
@@ -25,6 +24,7 @@ function operate(operator, num1, num2) {
 };
 
 /* Create the functions that populate the display when you click the number buttons. You should be storing the ‘display value’ in a variable somewhere for use in the next step. */
+
 console.log("----")
 
 let operation;
@@ -32,7 +32,6 @@ let display = document.getElementById('display');
 let allButtons = document.querySelectorAll('button')
 let currentDisplay;
 let lastElement;
-
 
 allButtons.forEach(function (e) {
     e.addEventListener('click', function () {
@@ -61,82 +60,55 @@ allButtons.forEach(function (e) {
                 currentDisplay = display.innerHTML;
             }
             //functionality for clicked operators
+
             // add functionality for comma button
         } else if (e.id == "comma") {
-            console.log("comma");
 
             let comma_temp = display.innerHTML.split(' ');
-            // don't show multiple commas for a single number e.g.: 2.3.4
-            
-
-
+            // don't add more than 1 comma for a single number e.g.: 2.3.4
             if (comma_temp.at(-1).includes('.') == false) {
-   /*              console.log("already at least one comma here")
-                //console.log(number1, operation);                
-                
-                /* const regex_comma =  /\./g;
-                let check = comma_temp.map(x => x.match(regex_comma));
-                console.log("check: ", check);
-                console.log("check length: ", check[0].length) 
-                
-                 
-                if (check[0] != undefined) {
-                    console.log(check[h].toString() + " already has a comma");
-                } else {
-                    display.innerHTML += e.innerHTML;
-                    currentDisplay = display.innerHTML;        
-                }; */
                 display.innerHTML += e.innerHTML;
                 currentDisplay = display.innerHTML;
-            } /* else {
-                display.innerHTML += e.innerHTML;
-                currentDisplay = display.innerHTML;
-            }; */
-        } else if (currentButton == "operator" && display.innerHTML != "") {
-
-            // calculate interim result
-            if (currentDisplay.toString().split(' ').length > 1) {
-                let temp = currentDisplay.split(' ');
-                console.log("tem", temp);
-                number1 = operate(temp[1], Number(temp[0]), Number(temp[2]));
-                //show error message if you try division by 0
-                if (number1 == Infinity) {
-                    alert("Error: Division by 0 not possible, please enter a valid calculation!");
-
-                } else {
-                    console.log("test: ", number1);
-                    display.innerHTML = Math.round(number1 * 100_000_0000) / 100_000_0000;
-                    operation = e.innerHTML
-                    currentDisplay = number1 + operation;
-                    display.innerHTML += operation;
-                };
-
-            } else {
-                number1 = currentDisplay;
-                console.log("num1: again;", number1.toString());
-                operation = e.innerHTML;
-                display.innerHTML += e.innerHTML;
-                currentDisplay = display.innerHTML;
-
-                console.log("currentDisplay: ", currentDisplay);
             };
+            // operator buttons - bug when / or * are clicked twice - NEED FIX
+        } else if (currentButton == "operator" && display.innerHTML != "") {
+            let operationLast = display.innerHTML.split(' ').at(-1)
+            console.log("last: ", operationLast);
+            if (operationLast != "+" && operationLast != "-" && operationLast != "×"
+                && operationLast != "÷" && operationLast != '') {
 
+                // calculate interim result
+                if (currentDisplay.toString().split(' ').length > 1) {
+                    let temp = currentDisplay.split(' ');
+                    console.log("tem", temp);
+                    number1 = operate(temp[1], Number(temp[0]), Number(temp[2]));
+                    console.log(temp[2]);
+                    //show error message if you try division by 0
+                    if (number1 == Infinity) {
+                        alert("Error: Division by 0 not possible, please enter a valid calculation!");
+                    } else {
+                        console.log("test: ", number1);
+                        display.innerHTML = Math.round(number1 * 100_000_0000) / 100_000_0000;
+                        operation = e.innerHTML
+                        currentDisplay = number1 + operation;
+                        display.innerHTML += operation;
+                    };
+                } else {
+                    number1 = currentDisplay;
+                    console.log("num1: again;", number1.toString());
+                    operation = e.innerHTML;
+                    display.innerHTML += e.innerHTML;
+                    currentDisplay = display.innerHTML;
+                    console.log("currentDisplay: ", currentDisplay);
+                };
+            }
+
+            // result button
         } else if (e.id == "getResult") {
             if (display.innerHTML === '') {
                 alert("No inputs give, please enter a valid calculation")
             } else {
                 console.log("number 1 before: ", number1.toString());
-
-                /*             if(isNaN(Number(number1))) {
-                                console.log("is nan: ", number1);
-                                let temp = number1.split(" ");
-                                console.log("calculate this first", temp)
-                                number1 = operate(temp[1], Number(temp[0]), Number(temp[2]));
-                                display.innerHTML = number1;
-                            } else {
-                                console.log("not nan");
-                            }; */
-
                 number1 = Number(number1);
                 number2 = Number(currentDisplay.split(" ").at(-1));
 
@@ -151,13 +123,13 @@ allButtons.forEach(function (e) {
                     currentDisplay = result;
                     display.innerHTML = Math.round(currentDisplay * 100_000_0000) / 100_000_0000;
                     console.log("result: ", result.toString());
-                }
-            }
+                };
+            };
         };
-
         console.log(currentDisplay);
-    });
+    })
 });
+
 
 function fillDisplay() {
     let digits = document.body.getElementsByClassName('digit')
